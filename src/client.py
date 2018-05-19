075abc2e441299ef758b9abbe062f3ad207962a0
Joe Kottke <joe.kottke@gmail.com>
2018-05-18 17:42:26 -0700

    Changed client. Added host and port options.

--- src/client.py
+++ src/client.py
@@ -1,8 +1,14 @@
+import argparse
 import random
 import grpc
 
 import namer_pb2
 import namer_pb2_grpc
+
+parser = argparse.ArgumentParser(description='GRPC-based namer client.')
+parser.add_argument('--port', type=int, required=True, help='The server port')
+parser.add_argument('--host', type=str, required=True, help='The server hostname or address')
+args = parser.parse_args()
 
 prefixes = ['Dr.', 'Honorary', 'Judge']
 suffixes = ['III', 'II', 'The Great', 'BDFL', 'Esq']
@@ -11,13 +17,13 @@ first_names = ['Jim', 'Henry', 'Michael', 'Robert', 'Ronald',
 last_names = ['Smith', 'Jones', 'Cooper', 'Hunter', 'Baker',
               'Watson', 'Redmond', 'Williams', 'Crick', 'Moore']
 
-channel = grpc.insecure_channel('localhost:454545')
+channel = grpc.insecure_channel(args.host + ':' + str(args.port))
 
 stub = namer_pb2_grpc.NamerStub(channel)
 
 for i in xrange(1000):
-  first = first_names[random.randint(0,9)] 
-  last = last_names[random.randint(0,9)] 
+  first = first_names[random.randint(0,9)]
+  last = last_names[random.randint(0,9)]
   name_request = namer_pb2.NameRequest(first_name=first, last_name=last)
   name_response = stub.EnglishFullName(name_request)
   print("Got response: '{}'".format(name_response.full_name))
