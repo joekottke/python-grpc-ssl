b4e2d19bd98641c160e6321a4b5d683c22606705
Joe Kottke <joe.kottke@gmail.com>
2018-05-21 15:52:03 -0700

    Update namer.py with cleaner formatting

--- src/namer.py
+++ src/namer.py
@@ -1,18 +1,19 @@
-def english_full_name(first=None, last=None, middle=None, prefix=None, suffix=None):
-  fullname = None;
+def english_full_name(first=None, last=None, middle=None,
+                      prefix=None, suffix=None):
+    fullname = None
 
-  if first is None or last is None:
-    raise ValueError("first and last must be specified")
+    if first is None or last is None:
+        raise ValueError("first and last must be specified")
 
-  if middle:
-    fullname = first + " " + middle + " " + last
-  else:
-    fullname = "{} {}".format(first, last)
+    if middle:
+        fullname = first + " " + middle + " " + last
+    else:
+        fullname = "{} {}".format(first, last)
 
-  if prefix:
-    fullname = prefix + " " + fullname
+    if prefix:
+        fullname = prefix + " " + fullname
 
-  if suffix:
-    fullname = fullname + " " + suffix
+    if suffix:
+        fullname = fullname + " " + suffix
 
-  return fullname
+    return fullname
