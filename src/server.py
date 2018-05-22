7f8993cddfd52055efadd5d2a82533518726d61c
Joe Kottke <joe.kottke@gmail.com>
2018-05-22 13:36:01 -0700

    Update server.py: cli arguments, refactored

--- src/server.py
+++ src/server.py
@@ -22,25 +22,65 @@ class Namer(namer_pb2_grpc.NamerServicer):
             first=first, last=last,
             middle=middle, prefix=prefix,
             suffix=suffix)
-        print('Peer: {}\nPeerIdentity: {}\nMetadata: {}'.format(
+        print('Peer: {}\nPeerIdentityKey: {}\nMetadata: {}'.format(
             context.peer(), context.peer_identity_key(),
             context.invocation_metadata()))
         return response
 
 
-def serve(port=31000):
-    private_key = open('../ssl/server-key.pem').read()
-    certificate_chain = open('../ssl/server.pem').read()
-    ca_cert = open('../ssl/ca.pem').read()
-    credentials = grpc.ssl_server_credentials(
-        [(private_key, certificate_chain)],
-        root_certificates=ca_cert,
-        require_client_auth=True
+def command_args():
+    parser = argparse.ArgumentParser(description='GRPC-based namer server.')
+    parser.add_argument(
+        '--port',
+        type=int,
+        default=31000,
+        help='The server listen port'
     )
+    parser.add_argument(
+        '--use_tls_auth',
+        action='store_true',
+        help='Enable TLS Connectivity.'
+    )
+    parser.add_argument(
+        '--ca_cert',
+        type=str,
+        help='CA cert or bundle.'
+    )
+    parser.add_argument(
+        '--server_cert',
+        type=str,
+        help='Server certificate.'
+    )
+    parser.add_argument(
+        '--server_key',
+        type=str,
+        help='Server certificate key.'
+    )
+    return parser.parse_args()
+
+
+def serve(args):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     namer_pb2_grpc.add_NamerServicer_to_server(Namer(), server)
-    print('Starting server. Listening on port {}...'.format(port))
-    server.add_secure_port('[::]:' + str(port), credentials)
+
+    if args.server_key and args.server_cert:
+        private_key = open(args.server_key).read()
+        certificate_chain = open(args.server_cert).read()
+        if args.use_tls_auth:
+            ca_cert = open(args.ca_cert).read()
+            credentials = grpc.ssl_server_credentials(
+                [(private_key, certificate_chain)],
+                root_certificates=ca_cert,
+                require_client_auth=True
+            )
+        else:
+            credentials = grpc.ssl_server_credentials(
+                [(private_key, certificate_chain)]
+            )
+        server.add_secure_port('[::]:' + str(args.port), credentials)
+    else:
+        server.add_insecure_port('[::]:' + str(args.port))
+    print('Starting server. Listening on port {}...'.format(args.port))
     server.start()
     try:
         while True:
@@ -50,9 +90,5 @@ def serve(port=31000):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='GRPC-based namer server.')
-    parser.add_argument(
-        '--port', type=int, default=31000,
-        help='The server port')
-    args = parser.parse_args()
-    serve(args.port)
+    args = command_args()
+    serve(args)
